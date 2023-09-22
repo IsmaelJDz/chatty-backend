c4d2ee562d1c8a04f3ae3daafa9de8a42f8be779
Ismael R <ismael_br7@hotmail.com>
2023-09-21 23:57:05 -0600

    feature: added post fatures

--- src/shared/services/db/post.service.ts
+++ src/shared/services/db/post.service.ts
@@ -63,6 +63,14 @@ class PostService {
 
     await Promise.all([deletePost, decrementPostCount]);
   }
+
+  public async editPost(postId: string, updatedPost: IPostDocument): Promise<void> {
+    const updatePost: UpdateQuery<IPostDocument> = PostModel.updateOne(
+      { _id: postId },
+      { $set: updatedPost }
+    );
+    await Promise.all([updatePost]);
+  }
 }
 
 export const postService: PostService = new PostService();
